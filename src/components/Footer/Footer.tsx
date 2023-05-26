472e804ea52aa9f82169ea5763396d0fb92b9d98
Marco Villanueva <marco.villanueva@globant.com>
2023-05-26 13:13:12 -0600

    feat( footer ): add footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,18 @@
 import { component$ } from "@builder.io/qwik";
+import styles from './Footer.module.css';
 
 export const Footer = component$(() => {
   return (
-    <>
-      <div>Footer</div>
-    </>
+    <div class={styles.footer}>
+      <section class={styles['address-container']} >
+        <span class={styles['address-title']}>Visítanos en:</span>
+        <span class={styles['address-text']}>Privada Sol #7 Col. Chapultepec, Cuernavaca, Morelos, México. C.P. 62450</span>
+        <span class={styles['address-link']}>Búscanos en Google Maps</span>
+      </section>
+      <section class={styles['phone-container']} >
+        <span class={styles['phone-title']} >Contáctanos: </span>
+        <span class={styles['phone-text']} >WhatsApp: +52 (777) 608-9602</span>
+      </section>
+    </div>
   )
 });
